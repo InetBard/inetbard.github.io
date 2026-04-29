2e53e5208f6e1f97aa76b6f4bae5e07dcdb561d7
InetBard <inetbard@gmail.com>
2026-04-29 17:01:43 -0600

    Replace image paths with imported variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useEffect } from "react";
+import avatar from './assets/images/pngtree-cartoon-character-hand-made-model-with-big-ears-png-image-7076952-96x96.png';
+import portrait from './assets/images/project3.png';
+
 import {
   NavLink,
   Route,
@@ -55,7 +58,7 @@ function Header() {
     <header className="topbar">
       <NavLink className="brand" to="/">
         <img
-          src="./assets/images/pngtree-cartoon-character-hand-made-model-with-big-ears-png-image-7076952-96x96.png"
+          src={avatar}
           alt="Avatar logo"
         />
         <span>AB</span>
@@ -124,7 +127,7 @@ function HomePage() {
         <div className="hero-visual">
           <div className="hero-visual-frame">
             <img
-              src="./assets/images/project3.png"
+              src={portrait}
               alt="Cyberpunk inspired portrait"
             />
           </div>
